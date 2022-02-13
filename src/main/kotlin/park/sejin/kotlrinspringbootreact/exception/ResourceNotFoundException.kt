97c4ebcf0004d83faeb2b033be4351dbfb1b7967
Sejin-P <chunpark37@gmail.com>
2022-02-13 22:03:56 +0900

    create not found exception

---
+++ src/main/kotlin/park/sejin/kotlrinspringbootreact/exception/ResourceNotFoundException.kt
@@ -0,0 +1,14 @@
+package park.sejin.kotlrinspringbootreact.exception
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+class ResourceNotFoundException(
+    override val message: String
+) : RuntimeException() {
+    companion object {
+        const val serialVersionUID: Long = 1L
+    }
+
+}
