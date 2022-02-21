d3fe3f34750a9e15bc1f5ac714c9851a25e4f673
sejin-P <chunpark37@gmail.com>
2022-02-21 21:35:18 +0900

    cors policy

--- src/main/kotlin/park/sejin/kotlrinspringbootreact/controller/EmployeeController.kt
+++ src/main/kotlin/park/sejin/kotlrinspringbootreact/controller/EmployeeController.kt
@@ -1,12 +1,14 @@
 package park.sejin.kotlrinspringbootreact.controller
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import park.sejin.kotlrinspringbootreact.model.Employee
 import park.sejin.kotlrinspringbootreact.repository.EmployeeRepository
 
+@CrossOrigin(origins = ["http://localhost:3000"])
 @RestController
 @RequestMapping("/api/v1/")
 class EmployeeController(
