3cb2999ec32a12c9a7c5c796249afdf763340d6b
sejin-P <chunpark37@gmail.com>
2022-03-01 17:31:29 +0900

    get employee by id

--- src/main/kotlin/park/sejin/kotlrinspringbootreact/controller/EmployeeController.kt
+++ src/main/kotlin/park/sejin/kotlrinspringbootreact/controller/EmployeeController.kt
@@ -1,10 +1,14 @@
 package park.sejin.kotlrinspringbootreact.controller
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
+import park.sejin.kotlrinspringbootreact.exception.ResourceNotFoundException
 import park.sejin.kotlrinspringbootreact.model.Employee
 import park.sejin.kotlrinspringbootreact.repository.EmployeeRepository
+import javax.persistence.EntityNotFoundException
 
 @CrossOrigin(origins = ["http://localhost:3000"])
 @RestController
@@ -24,5 +28,17 @@ class EmployeeController(
         employee: Employee
     ): Employee {
         return employeeRepository.save(employee)
+    }
+
+    @GetMapping("/employees/{id}")
+    fun getEmployee(
+        @PathVariable id: Long
+    ): ResponseEntity<Employee> {
+        try {
+            val employee = employeeRepository.getById(id)
+            return ResponseEntity.ok(employee)
+        } catch (e: EntityNotFoundException) {
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, "employee $id")
+        }
     }
 }
