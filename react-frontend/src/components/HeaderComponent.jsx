b4cd2ae5802913cac35ac0defc5faf108c2cc6bc
sejin-P <chunpark37@gmail.com>
2022-03-06 21:12:35 +0900

    implement header component

--- react-frontend/src/components/HeaderComponent.jsx
+++ react-frontend/src/components/HeaderComponent.jsx
@@ -4,7 +4,15 @@ class HeaderComponent extends Component {
     render() {
         return (
             <div>
-                
+                <header>
+                    <nav className={"navbar navbar-expand-md navbar-dark bg-dark"}>
+                        <div>
+                            <a href={"https://javaguides.net"} className={"navbar-brand"}>
+                                Employee Management App
+                            </a>
+                        </div>
+                    </nav>
+                </header>
             </div>
         );
     }
